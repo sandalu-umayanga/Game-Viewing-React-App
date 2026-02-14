e801ab506c2edc40231fcfc19ce0ad1285f42227
Sandalu Umayanga <sandaluumayanga2002@gmail.com>
2026-02-14 19:55:33 +0530

    basic foundation has been built

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 
@@ -17,11 +17,9 @@ function App() {
       <GridItem area="nav">
         <NavBar />
       </GridItem>
-      <Show above="lg">
-        <GridItem area="aside" paddingX={5}>
-          Aside
-        </GridItem>
-      </Show>
+      <GridItem area="aside" paddingX={5} display={{ base: "none", lg: "block" }}>
+        Aside
+      </GridItem>
       <GridItem area="main">
         <GameGrid />
       </GridItem>
